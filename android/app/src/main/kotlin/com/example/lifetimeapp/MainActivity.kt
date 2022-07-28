a68611bc006586eedd02c594ec8a272b3f884047
0x1772 <realhateux@outlook.com>
2022-07-28 12:41:21 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/lifetimeapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lifetimeapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'YAŞAM BEKLENTİSİ',
+          style: TextStyle(color: Colors.black54),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Form Alanı'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
